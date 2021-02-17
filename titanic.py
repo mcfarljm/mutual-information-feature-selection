abb5c34848e3ea313fd3dd69c8a33b49c2bdf44f
John McFarland <jmcfarland@dwavesys.com>
2021-02-17 11:30:40 -0800

    Switch to SimulatedAnnealingSampler

--- titanic.py
+++ titanic.py
@@ -24,7 +24,8 @@ import pandas as pd
 
 # D-Wave Ocean tools
 import dimod
-from dwave.system import DWaveCliqueSampler
+# from dwave.system import DWaveCliqueSampler
+from neal import SimulatedAnnealingSampler
 
 
 # Define MI calculations
@@ -139,7 +140,8 @@ def run_demo():
         bqm.add_interaction(f1, f0, -cmi_10)
 
     # Set up a QPU sampler with a fully-connected graph of all the variables
-    sampler = DWaveCliqueSampler()
+    # sampler = DWaveCliqueSampler()
+    sampler = SimulatedAnnealingSampler()
 
     # For each number of features, k, penalize selection of fewer or more features
     selected_features = np.zeros((len(features), len(features)))
